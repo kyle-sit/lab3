33cc4f6980cd2c5b3bc77981b2a0e433017575be
Kyle Sit <ktsit@ucsd.edu>
2018-01-24 22:03:37 -0800

    first 50 slides

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,28 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("Please wait... Asuh dude");
+		$(".jumbotron p").addClass("active");
 	});
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.fadeOut();
+    }
+}
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$(".project").click(projectClick);
 }
